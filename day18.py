51e58c912463fb5017c679708ad0d87dbb88651c
guillaume teissier <201614+gteissier@users.noreply.github.com>
2021-12-18 17:05:37 +0100

    add comments

--- day18.py
+++ day18.py
@@ -4,6 +4,7 @@ import itertools
 import sys
 
 def walk(l, prefix=''):
+  '''generate all (path, regular) for a given list'''
   assert(type(l) == int or type(l) == list)
   if type(l) == int:
     yield (prefix, l)
@@ -14,6 +15,7 @@ def walk(l, prefix=''):
       yield x
 
 def is_pair(x,y):
+  '''pathx and pathy designate a pair iff pathx and pathy overlap except for the last component'''
   if len(x) != len(y): return False
   return x[:-1] == y[:-1]
 
@@ -23,6 +25,9 @@ assert(not is_pair('llr', 'rll'))
 assert(not is_pair('llr', 'l'))
 
 class Snail:
+  '''Snail are just a collection of (path, regular) tuples
+  We use a dictionary, with path as key
+  '''
   def __init__(self, l):
     self.content = {}
 
@@ -34,6 +39,11 @@ class Snail:
     assert(path + 'l' in self.content)
     assert(path + 'r' in self.content)
 
+    # several things to know here:
+    # sorted uses lexicographic order for string: l < lr < r
+    # if we sort paths, and find our path inside
+    # we have the prev and the next (possibly)
+    # and they coincide with the elements we want to update
     keys = sorted(k for k in self.content.keys())
     vl = self.content[path + 'l']
     ndx = keys.index(path + 'l')
@@ -45,9 +55,11 @@ class Snail:
     if ndx != len(keys)-1:
       self.content[keys[ndx+1]] += vr
 
+    # cut the old nodes
     del self.content[path + 'l']
     del self.content[path + 'r']
 
+    # add a new node with value 0
     self.content[path] = 0
 
   def split(self, greater):
@@ -56,33 +68,43 @@ class Snail:
       vl, vr = value//2, value//2
     else:
       vl, vr = value//2, value//2+1
+      
+    # cut old node
     del self.content[greater]
+    
+    # add two child nodes with computed values
     self.content[greater + 'l'] = vl
     self.content[greater + 'r'] = vr
 
   def reduce_once(self):
     # explode
+    # to find level 4 pairs, we find regulars at level 5
     five_paths = sorted([k for k in self.content.keys() if len(k) == 5])
-    # all paths are exactly 5
+    # and find the first pair
+    # sorted ensures we have the leftmost one (again lexicographic order)
     for i in range(0, len(five_paths)-1):
       if is_pair(five_paths[i], five_paths[i+1]):
         self.explode(five_paths[i][:-1])
         return True
 
     # split
+    # find path for regulars whose values is greater or equal to ten
     greater = sorted([k for (k,v) in self.content.items() if v >= 10])
     if greater:
+      # sorted ensure we have the leftmost one (again lexicographic order)
       self.split(greater[0])
       return True
 
     return False
 
   def reduce(self):
+    '''reduce step by step until not possible'''
     while True:
       if not self.reduce_once():
         break
 
   def __add__(self, other):
+    '''to create a new tree, we prefix existing paths with 'l' or 'r''''
     content = {}
     for (k, v) in self.content.items():
       content['l' + k] = v
@@ -93,6 +115,7 @@ class Snail:
     return self
 
   def to_list(self):
+    '''crappy: we build values from bottom, pair by pair. Yet it works'''
     values = dict(self.content)
     while len(values) > 1:
       start = len(values)
@@ -115,6 +138,7 @@ class Snail:
     return values['']
 
   def magnitude(self):
+    '''the same, but evaluates the tree w.r.t. to magnitude'''
     values = dict(self.content)
     while len(values) > 1:
       start = len(values)
@@ -140,21 +164,26 @@ class Snail:
     return '%r' % (self.content)
 
 
+# verify magnitude works
 s = Snail(eval('[[1,2],[[3,4],5]]'))
 print(s.magnitude())
 assert(s.magnitude() == 143)
 
+
+# capture snail expressions
 exprs = []
 for line in sys.stdin.readlines():
   line = line.rstrip('\n')
   exprs.append(line)
 
+# sum up snails
 total = Snail(eval(exprs[0]))
 for i in range(1, len(exprs)):
   total = total + Snail(eval(exprs[i]))
 print(total.to_list())
 print(total.magnitude())
 
+# perform all r=2 permutations and get the maximum magnitude
 all_sums = []
 for p in itertools.permutations(range(len(exprs)), r=2):
   (i,j) = p
